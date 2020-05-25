fd28a91281c3f362b8e36c0e79f9d796fa79ce97
huntmori04x22 <huntmori04x22@gmail.com>
2020-05-26 06:08:54 +0900

    button enable-false, renaming buttons

--- Assets/Scripts/DAO/lobyDAO.cs
+++ Assets/Scripts/DAO/lobyDAO.cs
@@ -7,11 +7,17 @@ public class lobyDAO : MonoBehaviour
 {
 
     public Button btnTestMode;
+    public Button btnMatchStart;
+    public Button btnNotAllocated;
+    public Button btnConfig;
 
     // Start is called before the first frame update
     void Start()
     {
-        
+        btnTestMode.enabled = true;
+        btnMatchStart.enabled = false;
+        btnNotAllocated.enabled = false;
+        btnConfig.enabled = false;
     }
 
     // Update is called once per frame
