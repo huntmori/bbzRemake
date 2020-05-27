64367fd49b6172a5bf9a76336d6b100cbe713d67
huntmori <huntmori04x22@gmail.com>
2020-05-27 02:23:01 +0000

    replace server location

--- Assets/Scripts/DAO/indexDAO.cs
+++ Assets/Scripts/DAO/indexDAO.cs
@@ -30,7 +30,7 @@ public class indexDAO : MonoBehaviour
     // Start is called before the first frame update
     void Start()
     {
-        serverUrl = "http://49.175.158.186:58080";
+        serverUrl = "http://15.164.165.253:53526";
     }
 
 
