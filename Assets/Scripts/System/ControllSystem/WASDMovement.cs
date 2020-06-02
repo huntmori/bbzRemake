e8e27427b37dd6af05a79f779b80c91dc4b571ad
huntmori04x22 <huntmori04x22@gmail.com>
2020-06-02 23:14:12 +0900

    adjust movement, camera trace

--- Assets/Scripts/System/ControllSystem/WASDMovement.cs
+++ Assets/Scripts/System/ControllSystem/WASDMovement.cs
@@ -8,27 +8,13 @@ public class WASDMovement : MonoBehaviour
 {
     public float speed;
 
-    Rigidbody rigidbody;
-    Vector3 movement;
-    float h, v;
-    private void Start()
+    private void Update()
     {
-        rigidbody = GetComponent<Rigidbody>();
-    }
+        float h = Input.GetAxis("Horizontal");
+        float v = Input.GetAxis("Vertical");
 
-
-
-    void Update()
-    {
-        h = Input.GetAxisRaw("Horizontal");
-        h = Input.GetAxisRaw("Vertical");
-    }
-
-    private void FixedUpdate()
-    {
-        movement.Set(h, 0f, v);
-        movement = movement.normalized * speed * Time.deltaTime;
-        rigidbody.MovePosition(transform.position + movement);
+        transform.Translate(new Vector3(h, 0, v) * speed * Time.deltaTime);
+        transform.Rotate(new Vector3(0, Input.GetAxis("Mouse X")*5, 0));
     }
 
 }
