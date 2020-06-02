499ff727f734ef5609cdd14974b20bca086a164b
huntmori04x22 <huntmori04x22@gmail.com>
2020-06-02 14:06:27 +0900

    add movement script

---
+++ Assets/Scripts/System/ControllSystem/WASDMovement.cs
@@ -0,0 +1,34 @@
+﻿using UnityEngine;
+
+using System.Collections;
+
+
+
+public class WASDMovement : MonoBehaviour
+{
+    public float speed;
+
+    Rigidbody rigidbody;
+    Vector3 movement;
+    float h, v;
+    private void Start()
+    {
+        rigidbody = GetComponent<Rigidbody>();
+    }
+
+
+
+    void Update()
+    {
+        h = Input.GetAxisRaw("Horizontal");
+        h = Input.GetAxisRaw("Vertical");
+    }
+
+    private void FixedUpdate()
+    {
+        movement.Set(h, 0f, v);
+        movement = movement.normalized * speed * Time.deltaTime;
+        rigidbody.MovePosition(transform.position + movement);
+    }
+
+}
