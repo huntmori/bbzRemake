0729a7c985db95ce7ef92827bf402dc288c3cd49
huntmori04x22 <huntmori04x22@gmail.com>
2020-06-03 23:23:06 +0900

    update camera view script and chacter move script

--- Assets/Scripts/System/ControllSystem/WASDMovement.cs
+++ Assets/Scripts/System/ControllSystem/WASDMovement.cs
@@ -6,15 +6,48 @@ using System.Collections;
 
 public class WASDMovement : MonoBehaviour
 {
-    public float speed;
+    public float move_speed;
+    public float rotation_speed;
+
+    public Camera fpsCam;
+
+    private void Start()
+    {
+        
+    }
 
     private void Update()
     {
-        float h = Input.GetAxis("Horizontal");
-        float v = Input.GetAxis("Vertical");
-
-        transform.Translate(new Vector3(h, 0, v) * speed * Time.deltaTime);
-        transform.Rotate(new Vector3(0, Input.GetAxis("Mouse X")*5, 0));
+        MoveControll();
+        RotationControll();
     }
 
+    void MoveControll()
+    {
+        if (Input.GetKey(KeyCode.W))
+        {
+            this.transform.Translate(Vector3.forward * move_speed * Time.deltaTime);
+        }
+        if (Input.GetKey(KeyCode.S))
+        {
+            this.transform.Translate(Vector3.back * move_speed * Time.deltaTime);
+        }
+        if (Input.GetKey(KeyCode.A))
+        {
+            this.transform.Translate(Vector3.left * move_speed * Time.deltaTime);
+        }
+        if (Input.GetKey(KeyCode.D))
+        {
+            this.transform.Translate(Vector3.right * move_speed * Time.deltaTime);
+        }
+    }
+
+    void RotationControll()
+    {
+        float rotation_x = Input.GetAxis("Mouse Y") * rotation_speed;
+        float rotation_y = Input.GetAxis("Mouse X") * rotation_speed;
+
+        this.transform.localRotation *= Quaternion.Euler(0, rotation_y, 0);
+        fpsCam.transform.localRotation *= Quaternion.Euler(-rotation_x, 0, 0);
+    }
 }
