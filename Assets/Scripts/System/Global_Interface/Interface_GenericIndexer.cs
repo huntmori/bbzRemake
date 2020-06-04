287589729393ebaf53dc90fe969656a8fde8fd87
huntmori04x22 <huntmori04x22@gmail.com>
2020-06-04 22:37:10 +0900

    add Generic Indexer Interface

---
+++ Assets/Scripts/System/Global_Interface/Interface_GenericIndexer.cs
@@ -0,0 +1,9 @@
+﻿using UnityEngine;
+using System.Collections;
+
+// 인덱서 확장
+public interface Interface_GenericIndexer<ReturnType, IndexType>
+{
+    ReturnType this[IndexType index]
+    { get; set; }
+}
