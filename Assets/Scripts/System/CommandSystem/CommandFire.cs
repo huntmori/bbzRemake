3031fca409f7301653a51f6669ba31cbaf6048cc
huntmori04x22 <huntmori04x22@gmail.com>
2020-06-06 22:41:32 +0900

    add command pattern

---
+++ Assets/Scripts/System/CommandSystem/CommandFire.cs
@@ -0,0 +1,13 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using System.Windows.Input;
+using UnityEngine;
+
+
+public class CommandFire : CommandObject
+{
+    public void Execute()
+    {
+
+    }
+}
