3d5ce157e47f9d007400723af23fe160f0c4c00e
huntmori04x22 <huntmori04x22@gmail.com>
2020-06-10 23:42:31 +0900

    add CommandReload from CommandObject interface

---
+++ Assets/Scripts/System/CommandSystem/CommandReload.cs
@@ -0,0 +1,12 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using System.Windows.Input;
+using UnityEngine;
+public class CommandReload : CommandObject
+{
+
+    void CommandObject.Execute(CommandActor actor)
+    {
+        actor.Reload();
+    }
+}
