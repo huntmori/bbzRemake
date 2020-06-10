4579c99911b4ada7a8f6ee3ffe723d482bd4da02
huntmori04x22 <huntmori04x22@gmail.com>
2020-06-10 23:43:38 +0900

    add CommandEvade from CommandObject Interface

---
+++ Assets/Scripts/System/CommandSystem/CommandEvade.cs
@@ -0,0 +1,11 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using System.Windows.Input;
+using UnityEngine;
+public class CommandEvade : CommandObject
+{
+    void CommandObject.Execute(CommandActor actor)
+    {
+        actor.Evade();
+    }
+}
