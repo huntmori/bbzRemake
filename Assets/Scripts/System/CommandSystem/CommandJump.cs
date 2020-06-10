3a0754b60aa8e962ac0e2d6cd0bcfd669a173052
huntmori04x22 <huntmori04x22@gmail.com>
2020-06-10 23:43:03 +0900

    add CommandJump from CommandObject Interface

---
+++ Assets/Scripts/System/CommandSystem/CommandJump.cs
@@ -0,0 +1,12 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using System.Windows.Input;
+using UnityEngine;
+public class CommandJump : CommandObject
+{
+
+    void CommandObject.Execute(CommandActor actor)
+    {
+        actor.Jump();
+    }
+}
